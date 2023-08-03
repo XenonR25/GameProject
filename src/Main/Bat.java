00d8f457468407e01e4ecf1e04b61c5d4216adef
Farzine <alfezafarzine@gmail.com>
2023-08-04 01:48:07 +0600

    variable declare for bat

---
+++ src/Main/Bat.java
@@ -0,0 +1,18 @@
+package Main;
+
+import java.awt.Color;
+
+public class Bat
+{
+    // Instantiation of global instance
+    // variables for bat object
+    private Rectangle bat;
+    private GameWindow window;
+    private int batLength = 100;
+    private int batHeight = 20;
+    private int Right = 1;
+    private int Left = 2;
+    private int Direction;
+
+
+}
