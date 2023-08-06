1e0558ea42a8771373f1064f4780d44178ca30ce
NM Rahik <rahik2535@gmail.com>
2023-08-06 21:33:50 +0600

    Color Changing ball

--- src/Main/Oval.java
+++ src/Main/Oval.java
@@ -1,24 +1,38 @@
 package Main;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 
-    public class Oval extends JComponent
+public class Oval extends JComponent
     {
+        private Random random;
+
 
         public Oval( int x, int y, int w, int h )
         {
             super();
+
             setBounds( x, y, w, h );
-            setBackground( Color.green );
+            setBackground( Color.black);
+            random = new Random();
         }
 
 
         public void paint( Graphics g )
         {
-            g.setColor( getBackground() );
+
+
+            g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
             g.fillOval( 0, 0, getWidth(), getHeight() );
             paintChildren( g );
         }
-    }
+
+
+        }
+
+
+
+
+
 
